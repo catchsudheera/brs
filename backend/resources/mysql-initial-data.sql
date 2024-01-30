8c8e336a7344d78a8a09d20e65112c8e9ecbe0b0
sudheera <catchsudheera@gmail.com>
2024-01-30 23:15:51 +0100

    added initial data

---
+++ backend/resources/mysql-initial-data.sql
@@ -0,0 +1,22 @@
+INSERT INTO PLAYER (name,rank_score,player_rank) VALUES
+	 ('rajith',1000.0,1),
+	 ('nishan k',1000.0,2),
+	 ('shaki',1000.0,3),
+	 ('prabod',1000.0,4),
+	 ('supun',1000.0,5),
+	 ('shashika',1000.0,6),
+	 ('navanji',1000.0,7),
+	 ('chathura',1000.0,8),
+	 ('amila',1000.0,9),
+	 ('gihan',1000.0,10);
+INSERT INTO PLAYER (name,rank_score,player_rank) VALUES
+	 ('niranga',1000.0,11),
+	 ('nadeeshani',1000.0,12),
+	 ('waruni',1000.0,13),
+	 ('nishan w',1000.0,14),
+	 ('umesh',1000.0,15),
+	 ('tharindu',1000.0,16),
+	 ('sudheera',1000.0,17),
+	 ('darshana',1000.0,18),
+	 ('yeshan',1000.0,19),
+	 ('chamika',1000.0,20);
