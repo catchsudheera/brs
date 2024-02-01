30a1240c36e7bc54c97e827d922aa9d880215da4
Amila <amilabanuka@gmail.com>
2024-02-01 18:46:11 +0100

    Added Nipun and also updated the tired initial scores

--- backend/resources/mysql-initial-data.sql
+++ backend/resources/mysql-initial-data.sql
@@ -1,21 +1,35 @@
 INSERT INTO PLAYER (name,rank_score,player_rank) VALUES
-	 ('rajith',1000.0,19),
-	 ('nishan k',1000.0,5),
-	 ('shaki',1000.0,12),
-	 ('prabod',1000.0,17),
-	 ('supun',1000.0,13),
-	 ('shashika',1000.0,15),
-	 ('navanji',1000.0,14),
 	 ('chathura',1000.0,1),
-	 ('amila',1000.0,4),
-	 ('gihan',1000.0,8),
-	 ('niranga',1000.0,18),
-	 ('nadeeshani',1000.0,16),
-	 ('waruni',1000.0,6),
-	 ('nishan w',1000.0,9),
-	 ('umesh',1000.0,10),
-	 ('tharindu',1000.0,2),
-	 ('sudheera',1000.0,7),
-	 ('darshana',1000.0,3),
-	 ('yeshan',1000.0,11),
-	 ('chamika',1000.0,20);
+	 ('tharindu',952.24,2),
+	 ('darshana',928.73,3),
+	 ('amila',928.73,4),
+	 ('nishan k',927.95,5),
+	 ('waruni',927.13,6),
+	 ('sudheera',915.81,7),
+	 ('gihan',908.08,8),
+	 ('nishan w',906.09,9),
+	 ('umesh',897.16,10),
+	 ('yeshan',882.56,11),
+	 ('shaki',868.01,12),
+	 ('supun',867.62,13),
+	 ('navanji',862.76,14),
+	 ('shashika',854.26,15),
+	 ('nadeeshani',849.40,16),
+	 ('prabod',842.5,17),
+	 ('niranga',820.26,18),
+	 ('rajith',814.57,19),
+	 ('chamika',800.0,20),
+	 ('nipun',820.26,20);
+
+
+
+
+
+
+
+
+
+
+
+
+
