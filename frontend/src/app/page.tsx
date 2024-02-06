d6d4e72f1284d567b2b6d4cd9933d0e6e0a74b01
sudheera <catchsudheera@gmail.com>
2024-02-06 20:52:06 +0100

    - Added image build and push steps  - Updated Deployment config

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,16 +2,17 @@
 import {Card, Typography} from "@material-tailwind/react";
 
 
+// eslint-disable-next-line @next/next/no-async-client-component
 export default async function Page() {
 
     const data = await getPlayerRankingData();
-    data.sort((a,b) => a.playerRank - b.playerRank);
+    data.sort((a: { playerRank: number }, b: { playerRank: number }) => a.playerRank - b.playerRank);
     const tableRows = data;
     const tableHead = ["Player Id", "Player Name", "Score", "Rank",];
 
     return (
         <div className="place-items-center">
-            <Card className="h-full w-full overflow-scroll">
+            <Card className="h-full w-full overflow-scroll" placeholder={undefined}>
                 <table className="table-auto text-left">
                     <thead>
                     <tr>
@@ -21,7 +22,7 @@ export default async function Page() {
                                     variant="small"
                                     color="blue-gray"
                                     className="font-normal leading-none opacity-70"
-                                >
+                                    placeholder={undefined}                                >
                                     {head}
                                 </Typography>
                             </th>
@@ -29,29 +30,29 @@ export default async function Page() {
                     </tr>
                     </thead>
                     <tbody>
-                    {tableRows.map(({id, name, rankScore, playerRank}, index) => {
+                    {tableRows.map(({id, name, rankScore, playerRank}: {id: string, name: string, rankScore: number, playerRank: number}, index: number) => {
                         const isLast = index === tableRows.length - 1;
                         const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
 
                         return (
                             <tr key={id}>
                                 <td className={classes}>
-                                    <Typography variant="small" color="blue-gray" className="font-normal">
+                                    <Typography variant="small" color="blue-gray" className="font-normal" placeholder={undefined} >
                                         {id}
                                     </Typography>
                                 </td>
                                 <td className={classes}>
-                                    <Typography variant="small" color="blue-gray" className="font-normal">
+                                    <Typography variant="small" color="blue-gray" className="font-normal" placeholder={undefined} >
                                         {name.charAt(0).toUpperCase() + name.slice(1)}
                                     </Typography>
                                 </td>
                                 <td className={`${classes} bg-blue-gray-50/50`}>
-                                    <Typography variant="small" color="blue-gray" className="font-normal">
+                                    <Typography variant="small" color="blue-gray" className="font-normal" placeholder={undefined} >
                                         {Math.round(rankScore * 100) / 100}
                                     </Typography>
                                 </td>
                                 <td className={classes}>
-                                    <Typography variant="small" color="blue-gray" className="font-normal">
+                                    <Typography variant="small" color="blue-gray" className="font-normal" placeholder={undefined} >
                                         {playerRank}
                                     </Typography>
                                 </td>
