82e4a44b0077e55619b76a3ff70b1bcd5dba5362
Nishan Karunarathna <nkarunarathna@harlemnext.com>
2024-02-10 18:34:28 +0100

    Link logo

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 const Navigation = () => {
   return (
     <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <div className="text-lg font-semibold">Badminton Almere</div>
+      <Link href="/">
+        <div className="text-lg font-semibold">Badminton Almere</div>
+      </Link>
       <ul className="flex space-x-4">
         <li>
           <Link href="/" className="hover:text-gray-200">
