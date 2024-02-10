6da817051fdff19d23b3fca93a88cef9aba80100
Nishan Karunarathna <nkarunarathna@harlemnext.com>
2024-02-10 17:06:11 +0100

    Add encounters page

---
+++ frontend/src/components/PlayerEncounterComponent.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Encounter, EncounterPlayer } from "@/types/encounter";
+import { capitalizeFirstLetter } from "@/utils/string";
+
+interface PlayerEncounterComponentProps {
+  encounter: Encounter;
+}
+
+const renderTeamList = (team: EncounterPlayer[]) =>
+  team
+    .map(
+      (player, index) =>
+        `${capitalizeFirstLetter(player.playerName)}${
+          index < team.length - 1 ? ", " : ""
+        }`
+    )
+    .join("");
+
+const PlayerEncounterComponent: React.FC<PlayerEncounterComponentProps> = ({
+  encounter,
+}) => {
+  return (
+    <div className="card bg-base-100 shadow-xl mb-8">
+      <div className="card-body">
+        <div className="stats shadow">
+          <div className="stat place-items-center">
+            <div className="stat-title">{encounter.encounterDate}</div>
+            <div className="stat-value">{`Game ${encounter.encounterId}`}</div>
+          </div>
+
+          <div className="stat place-items-center">
+            <div className="stat-title">Score</div>
+            <div className="stat-value">{encounter.encounterScore}</div>
+          </div>
+        </div>
+        <table className="table-auto w-full">
+          <thead>
+            <tr>
+              <th className="border px-4 py-2">Players</th>
+              <th className="border px-4 py-2">Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border px-4 py-2">
+                {renderTeamList(encounter.playerTeam)}
+              </td>
+              <td className="border px-4 py-2 text-center">
+                {encounter.playerTeamPoints}
+              </td>
+            </tr>
+            <tr className="bg-gray-100">
+              <td className="border px-4 py-2 text-center" colSpan={2}>
+                vs
+              </td>
+            </tr>
+            <tr>
+              <td className="border px-4 py-2">
+                {renderTeamList(encounter.opponentTeam)}
+              </td>
+              <td className="border px-4 py-2 text-center">
+                {encounter.opponentTeamPoints}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default PlayerEncounterComponent;
---
+++ frontend/src/components/PlayerEncountersComponent.client.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { EncountersResponse } from "@/types/encounter";
+import PlayerEncounterComponent from "./PlayerEncounterComponent";
+
+interface PlayerEncountersComponentProps {
+  playerId: string | string[] | undefined;
+}
+
+const PlayerEncountersComponent: React.FC<PlayerEncountersComponentProps> = ({
+  playerId,
+}) => {
+  const [encounters, setEncounters] = useState<EncountersResponse | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchEncounters = async () => {
+      if (!playerId) return;
+      setLoading(true);
+      try {
+        const response = await axios.get<EncountersResponse>(
+          `https://brs.aragorn-media-server.duckdns.org/players/${playerId}/encounters`
+        );
+        setEncounters(response.data);
+      } catch (error) {
+        console.error(
+          "There was an error fetching the encounters data:",
+          error
+        );
+        setError("Failed to fetch encounters data.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEncounters();
+  }, [playerId]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold text-center my-4">
+        Player Encounters for {encounters?.playerName}
+      </h1>
+      {encounters?.encounterHistory.map((encounter) => (
+        <PlayerEncounterComponent
+          key={encounter.encounterId}
+          encounter={encounter}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default PlayerEncountersComponent;
