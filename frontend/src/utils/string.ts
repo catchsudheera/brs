9e094ba5e26dd22823328eb6ba8747bdbd622f07
Nishan Karunarathna <nkarunarathna@harlemnext.com>
2024-02-11 13:05:05 +0100

    Capitalize each word

--- frontend/src/utils/string.ts
+++ frontend/src/utils/string.ts
@@ -1,3 +1,6 @@
 export const capitalizeFirstLetter = (string: string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  return string
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
 };
