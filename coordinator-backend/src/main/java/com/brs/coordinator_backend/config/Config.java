896bae520581908f51e002193d8f4db6f63e1f8c
sudheera <catchsudheera@gmail.com>
2024-09-16 01:42:56 +0200

    Added poll state query function

--- coordinator-backend/src/main/java/com/brs/coordinator_backend/config/Config.java
+++ coordinator-backend/src/main/java/com/brs/coordinator_backend/config/Config.java
@@ -2,11 +2,19 @@ package com.brs.coordinator_backend.config;
 
 import com.brs.coordinator_backend.communication.config.EncounterEventListConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Config {
     
     @Autowired
     public EncounterEventListConfig EncounterEventListConfig;
+
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
