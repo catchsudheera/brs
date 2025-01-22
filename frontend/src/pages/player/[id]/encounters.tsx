de82b62112dfe5a7d5bd82d934ec4cf66d83eca5
Nishan Karunarathna <nkarunarathna@harlemnext.com>
2025-01-22 23:43:08 +0100

    More UI tweaks

--- frontend/src/pages/player/[id]/encounters.tsx
+++ frontend/src/pages/player/[id]/encounters.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import PlayerEncountersComponent from '@/components/PlayerEncountersComponent.client';
 import PlayerEncountersCompactComponent from '@/components/PlayerEncounterCompactComponent.client';
 
 const PlayerEncountersPage = () => {
