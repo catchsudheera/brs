890f252cc61c68e72365a150f1f44e77a15ef301
Nishan Karunarathna <nkarunarathna@harlemnext.com>
2025-01-31 18:15:33 +0100

    Add google domain to next config

---
+++ frontend/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com', // For Google profile images
+    ],
+  },
+}
+
+module.exports = nextConfig 
