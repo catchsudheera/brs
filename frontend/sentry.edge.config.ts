c7c53708966624b8b1e95ab3e2134fc4d651db69
Nishan Karunarathna <nkarunarathna@harlemnext.com>
2025-02-02 11:19:21 +0100

    Added sentry for debuggging

---
+++ frontend/sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://9bd73354a5607177e2683001b4601e3b@o4508749060440064.ingest.de.sentry.io/4508749062144080",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
