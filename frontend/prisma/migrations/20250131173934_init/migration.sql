4c6a11a8193140c66a86d8a16880113eadc070cb
Nishan Karunarathna <nkarunarathna@harlemnext.com>
2025-02-02 14:07:34 +0100

    Move to mysql

--- frontend/prisma/migrations/20250131173934_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Game" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "groups" JSONB NOT NULL,
-    "scores" JSONB NOT NULL DEFAULT '{}',
-    "status" TEXT NOT NULL DEFAULT 'DRAFT'
-);
